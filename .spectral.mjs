52f0505b1c0ba0aa0bcf6bd9b549a2d8d5e17422
Amit Panwar <woral88199@idwager.com>
2025-12-04 16:33:49 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwMDI0OQ/spectral.js?branch_slug=main&token=67c097b6-8b88-488c-a66e-cc63c934e9ad";
+export default { extends: ruleset };
